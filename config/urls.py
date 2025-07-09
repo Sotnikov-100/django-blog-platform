30d03786a7759dfd1bfa741b66af8d57286063b9
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-07-09 18:13:08 +0300

    setup main url configuration

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from users.views import CustomLoginView, custom_logout, RegisterView, ProfileView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("blog.urls")),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("user/logout/", custom_logout, name="logout"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
